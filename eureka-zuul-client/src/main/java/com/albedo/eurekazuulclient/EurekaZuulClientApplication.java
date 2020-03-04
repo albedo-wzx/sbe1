7d7055a82ab19370b26f741417a03381aa47ba7b
wangzhangxiong <18720061456@139.com>
2020-03-04 11:31:21 +0800

    add zuul module

---
+++ eureka-zuul-client/src/main/java/com/albedo/eurekazuulclient/EurekaZuulClientApplication.java
@@ -0,0 +1,17 @@
+package com.albedo.eurekazuulclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class EurekaZuulClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaZuulClientApplication.class, args);
+    }
+
+}
---
+++ eureka-zuul-client/src/test/java/com/albedo/eurekazuulclient/EurekaZuulClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albedo.eurekazuulclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaZuulClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
