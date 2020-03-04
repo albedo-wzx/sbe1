a15f10604a8a659c39795c9b7468f786cb11a974
wangzhangxiong <18720061456@139.com>
2020-03-04 10:50:26 +0800

    add turbine

---
+++ eureka-client-turbine/src/main/java/com/albedo/eurekaclientturbine/EurekaClientTurbineApplication.java
@@ -0,0 +1,13 @@
+package com.albedo.eurekaclientturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientTurbineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientTurbineApplication.class, args);
+    }
+
+}
---
+++ eureka-client-turbine/src/test/java/com/albedo/eurekaclientturbine/EurekaClientTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albedo.eurekaclientturbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientTurbineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
